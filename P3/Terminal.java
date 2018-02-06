b0dae0fe9d51ace3597dbe9bed21e43b5ec6f635
Raphael Chevallier <raphaelchevallier@hotmail.com>
2018-02-05 17:37:15 -0800

    Add files via upload
    The source code of the project

---
+++ P3/Terminal.java
@@ -0,0 +1,17 @@
+package P3;
+
+import java.util.Scanner;
+
+public class Terminal {
+	public static int input() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("? ");
+		return scan.nextInt();
+	}
+	public static void output(String msg) {
+		System.out.print("OUTPUT: " + msg);
+	}
+	public static void output(int msg) {
+		System.out.println("OUTPUT: " + msg);
+	}
+}
